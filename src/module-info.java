fd2d6bb94c64be4b00bc4fd0fdf0cd163ae4917b
Amol Gaikwad <risings017@gmail.com>
2025-03-31 08:49:12 +0530

    Added module related file.

--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,13 @@
 /**
- * 
- */
-/**
- * 
+ * A module in Java is a way to group related packages, resources, and a module
+ * descriptor. It was introduced in Java 9 as part of the Java Platform Module
+ * System (JPMS). The module descriptor, named module-info.java, specifies the
+ * module's name, dependencies, exported packages, provided services, consumed
+ * services, and allowed reflection. Modules enhance code organization,
+ * encapsulation, and maintainability by explicitly declaring dependencies and
+ * controlling access to packages. They improve application integrity and
+ * simplify the management of large projects by breaking them down into smaller,
+ * self-contained units.
  */
 module java_basic {
 }
