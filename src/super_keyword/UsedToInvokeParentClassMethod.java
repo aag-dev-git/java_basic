5dea2cd17b0c7549137351ae2622194f7b38bb27
Amol Gaikwad <risings017@gmail.com>
2025-04-07 08:38:51 +0530

    Added changes in super keyword related file.

--- src/super_keyword/UsedToInvokeParentClassMethod.java
+++ src/super_keyword/UsedToInvokeParentClassMethod.java
@@ -1,32 +1,45 @@
 package super_keyword;
+
 /**
- * It should be used if sub-class contains the same method
- * as the parent class, it is used if method overridden; 
+ * It should be used if sub-class contains the same method as the parent class,
+ * it is used if method overridden;
  */
-class ParentClass
-{
-	void show()
-	{
+class ParentClass {
+	void show() {
 		System.out.println("I'm Father");
 	}
 }
-class ChildClass extends ParentClass
-{
-	void show()
-	{
+
+/*
+ * The super keyword cannot be used to call static methods because static
+ * methods Protected and public methods can be accessed using super if they are
+ * inherited by the subclass. The super keyword can also be used to access
+ * parent class fields or constructors if needed
+ */
+class ChildClass extends ParentClass {
+	void show() {
 		System.out.println("I'm Child");
 	}
-	void work()
-	{
+
+	void work() {
 		super.show();
 		show();
 	}
+
+	/*
+	 * The super keyword cannot be used inside static methods or blocks since it
+	 * requires an instance context
+	 */
+
+	public static void workDone() {
+		// super.show(); // Compile error
+
+	}
 }
-public class UsedToInvokeParentClassMethod
-{
-	public static void main(String[] args)
-	{
-		ChildClass ch= new ChildClass();
+
+public class UsedToInvokeParentClassMethod {
+	public static void main(String[] args) {
+		ChildClass ch = new ChildClass();
 		ch.work();
 	}
 }
